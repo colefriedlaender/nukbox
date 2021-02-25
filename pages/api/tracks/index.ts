e243ed08cd4b1dc7301a43c9ac3e7eb2034b96da
colefriedlaender <colefriedlaender@outlook.de>
2021-02-25 17:08:46 +0100

    restored hooks

--- pages/api/tracks/index.ts
+++ pages/api/tracks/index.ts
@@ -1,7 +1,32 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { NextApiRequest, NextApiResponse } from "next";
-import db from "../../../server/db.json";
+import { readDb, writeDb } from "../../../server/db";
+import { APITrack } from "../../../utils/api";
 
-export default (_req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json(db.tracks);
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "GET") {
+    handleGet(req, res);
+  }
+  if (req.method === "POST") {
+    handlePost(req, res);
+  }
 };
+
+async function handleGet(req: NextApiRequest, res: NextApiResponse) {
+  const db = await readDb();
+  res.status(200).json(db.tracks);
+}
+
+async function handlePost(req: NextApiRequest, res: NextApiResponse) {
+  const newTrack: APITrack = req.body;
+
+  const db = await readDb();
+  if (db.tracks.some((track) => track.id === newTrack.id)) {
+    return res.status(409).json({
+      status: 409,
+    });
+  }
+  db.tracks = [...db.tracks, newTrack];
+  await writeDb(db);
+  res.status(201).json(newTrack);
+}
